7452531567cf727469c713f41447a4b213768e58
ostry1414 <ostry1414@gmail.com>
2019-11-28 19:18:39 +0100

    123

---
+++ src/com/sda/kolejka/KolejkaObj.java
@@ -0,0 +1,15 @@
+package com.sda.kolejka;
+
+public class KolejkaObj {
+    public void add(int liczba) {
+        new KolejkaElement(liczba);
+    }
+
+    public int pop() {
+        return 0;
+    }
+
+    public int push() {
+        return 0;
+    }
+}
--- src/com/sda/kolejka/Main.java
+++ src/com/sda/kolejka/Main.java
@@ -5,7 +5,8 @@ import java.util.Queue;
 public class Main {
     public static void main(String[] args) {
 
-       KolejkaLista kolejka = new KolejkaLista();
+       //KolejkaLista kolejka = new KolejkaLista();
+        KolejkaObj kolejka = new KolejkaObj();
 
        kolejka.add(8);
        kolejka.add(10);
