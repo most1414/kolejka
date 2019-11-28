d4894009d5ad7cdd710874f10d3428edb098b8c7
ostry1414 <ostry1414@gmail.com>
2019-11-28 20:27:00 +0100

    28.11

---
+++ src/com/sda/kolejka/Element.java
@@ -0,0 +1,13 @@
+package com.sda.kolejka;
+
+public class Element {
+    int value;
+
+    Element nastepny;
+
+    public Element(int liczba) {
+        value = liczba;
+        nastepny = null;
+    }
+
+}
--- src/com/sda/kolejka/KolejkaObj.java
+++ src/com/sda/kolejka/KolejkaObj.java
@@ -1,15 +1,51 @@
 package com.sda.kolejka;
 
 public class KolejkaObj {
+
+    Element pierwszy;
+    Element ostatni;
+
+    public KolejkaObj() {
+        pierwszy = null;
+        ostatni = null;
+    }
+
     public void add(int liczba) {
-        new KolejkaElement(liczba);
+        Element nowyElement = new Element(liczba);
+        Element przedostatni = ostatni;
+
+        if (ostatni == null)
+            pierwszy = ostatni = nowyElement;
+        else {
+            ostatni.nastepny = nowyElement;
+            ostatni = nowyElement;
+        }
     }
 
     public int pop() {
         return 0;
     }
 
+
+
     public int push() {
         return 0;
+    }
+    @Override
+    public String toString(){
+        String opis = null;
+       if (pierwszy != null){
+
+           Element bierzacyElement = pierwszy;
+
+           while (true){
+               opis = opis + bierzacyElement.value + " ";
+               bierzacyElement = bierzacyElement.nastepny;
+               if (bierzacyElement.nastepny == null) break;
+           }
+           return opis;
+       }else
+
+        return "Kolejka jest pusta";
     }
 }
